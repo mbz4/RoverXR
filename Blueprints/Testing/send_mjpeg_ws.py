81411b501b72296cf2b19b03acf68e449995487a
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 19:16:42 +0300

    wonder if this hot mess works

--- Blueprints/Testing/send_mjpeg_ws.py
+++ Blueprints/Testing/send_mjpeg_ws.py
@@ -8,38 +8,24 @@ import io
     raspivid -vf -hf -fps 30 -t 0 -l -w 1920 -h 1080 --codec MJPEG -o - | python3 send_mjpeg_ws.py
 '''
 async def send_mjpeg_stream(websocket):
-    try:   
-        boundary = b'\xff\xd8\r\n'
-        header = b'Content-Type: image/jpeg\r\n\r\n'
+    try:
+        marker = b'\xff\xd8'
+        trailer = b'\xff\xd9'
         while True:
-            buffer = io.BytesIO()
-            data = sys.stdin.buffer.read() # Read from stdin
+            image_buffer = io.BytesIO()
+            data = sys.stdin.buffer.readline() # Read from stdin until boundary marker
             if not data:
-                break
-            if data.startswith(b'\xff\xd8'):
-                buffer.seek(0)    
-                buffer.write(data)
-                # crop buffer to exact size
-                buffer.truncate()
-                # save the frame
-                frame = buffer.getvalue()
-                # wfile.write(b'--FRAME\r\n')
-                # send_header('Content-Type', 'image/jpeg')
-                # send_header('Content-Length', len(frame))
-                # wfile.write(frame)
-                # wfile.write(b'\r\n')
-                await websocket.send(frame)
-                
-                # data = sys.stdin.buffer.read() # Read the header and JPEG data
-                # if not data.startswith(header):
-                #     break
-                # jpeg_data = bytearray()
-                # while True:
-                #     data = sys.stdin.buffer.readline()
-                #     if data.startswith(boundary):
-                #         await websocket.send(jpeg_data) # Send the JPEG data over the WebSocket connection
-                #         break
-                #     jpeg_data.extend(data)
+                break        
+            if data.startswith(marker): # start of JPEG frame
+                while True:
+                    data = sys.stdin.buffer.readline()
+                    image_buffer.seek(0)
+                    image_buffer.write(data)
+                    image_buffer.truncate()
+                    frame = image_buffer.getvalue()
+                    if data.startswith(trailer): # end of JPEG frame
+                        await websocket.send(frame)
+                        break
     except websockets.exceptions.ConnectionClosedError:
         pass
 
