c5b892ba9f4cbb097e5bcd617236910e3e1ae8d9
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 22:40:43 +0300

    cleanup; it works!

--- Blueprints/Testing/argh.py
+++ Blueprints/Testing/argh.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-#modified from:
-# https://github.com/raspberrypi/picamera2/blob/main/examples/mjpeg_server.py
-#https://github.com/raspberrypi/picamera2/blob/main/examples/mjpeg_server_2.py
-# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
-# Run this script, then point a web browser at http:<this-ip-address>:8000
-# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
 
 import io
 from threading import Condition
@@ -26,23 +20,18 @@ class StreamingOutput(io.BufferedIOBase):
             self.condition.notify_all()
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}, transform=Transform(hflip=1, vflip=1))) 
+picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)}, transform=Transform(hflip=1, vflip=1))) 
 output = StreamingOutput()
-picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.LOW) #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps 
+picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.VERY_LOW) #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps 
 print('\033[2;31;43m \n\n\tRecording started\n\n\033[0;0m')
 
 async def handle_stream(websocket):
     global output
     try:
         while True:
-            
-            #print('\033[2;31;43m \n\n\tpinging client...\n\n\033[0;0m')
             with output.condition:
                 output.condition.wait()
                 frame = output.frame
-                
-            #print('\033[2;31;43m \n\n\tSending new frame\n\n\033[0;0m')
-            #print(frame)
             await websocket.send(frame)
     except Exception as e:
         print(e)
@@ -60,6 +49,7 @@ async def main():
     finally:
         picam2.stop_recording()
         picam2.close()
-
+        print('\033[2;31;43m \n\n\tRecording stopped\n\n\033[0;0m')
+        print("Exiting...")
+        quit()
 asyncio.run(main())
-
