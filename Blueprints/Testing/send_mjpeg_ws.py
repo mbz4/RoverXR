a85856645b432f5f6c46546ad0005e3d4ee2da09
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 19:42:01 +0300

    wait a minute

--- Blueprints/Testing/send_mjpeg_ws.py
+++ Blueprints/Testing/send_mjpeg_ws.py
@@ -2,7 +2,7 @@
 import asyncio
 import websockets
 import sys
-import io
+# import io
 '''
     Intended to be run from terminal as: 
     raspivid -vf -hf -fps 30 -t 0 -l -w 1920 -h 1080 --codec MJPEG -o - | python3 send_mjpeg_ws.py
@@ -19,17 +19,24 @@ async def handle_stream(websocket):
                 break        
             if data.startswith(marker): # start of JPEG frame
                 print("Start of JPEG frame")
+                frame = bytearray()
                 while True:
                     data = sys.stdin.buffer.readline()
-                    # print(data)
-                    image_buffer.seek(0)
-                    image_buffer.write(data)
-                    image_buffer.truncate()
-                    frame = image_buffer.getvalue()
-                    if data.startswith(trailer): # end of JPEG frame
+                    if data.startswith(marker):
+                        # Send the JPEG data over the WebSocket connection
                         await websocket.send(frame)
-                        print("\n\tSent frame\n")
                         break
+                    frame.extend(data)
+                    # data = sys.stdin.buffer.readline()
+                    # # print(data)
+                    # image_buffer.seek(0)
+                    # image_buffer.write(data)
+                    # image_buffer.truncate()
+                    # frame = image_buffer.getvalue()
+                    # if data.startswith(trailer): # end of JPEG frame
+                    #     await websocket.send(frame)
+                    #     print("\n\tSent frame\n")
+                    #     break
     except websockets.exceptions.ConnectionClosedError:
         pass
 
