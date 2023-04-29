3cf78ab8ce218c1e062bf4c684c78a5ad3c783ab
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 21:48:52 +0300

    cleaned up

--- Blueprints/Testing/argh.py
+++ Blueprints/Testing/argh.py
@@ -7,9 +7,6 @@
 # Note: needs simplejpeg to be installed (pip3 install simplejpeg).
 
 import io
-import logging
-import socketserver
-from http import server
 from threading import Condition
 from websockets.server import serve
 import asyncio
@@ -18,49 +15,6 @@ from picamera2.encoders import MJPEGEncoder, Quality
 from picamera2.outputs import FileOutput
 from libcamera import Transform
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
 
 
 class StreamingOutput(io.BufferedIOBase):
@@ -82,12 +36,7 @@ async def handle_stream(websocket):
                 frame = output.frame
             await websocket.send(frame)
     except Exception as e:
-        logging.warning('Removed streaming client %s: %s', client_address, str(e))
-    
-    async for message in websocket:
-        message = message.decode("utf-8")
-        print(f"Message from client: {message}")
-        await websocket.send(f"Received your message: {message}")
+        print(e)
     
 picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}, transform=Transform(hflip=1, vflip=1))) 
@@ -99,9 +48,11 @@ async def main():
         async with serve(handle_stream, "localhost", 3333):
             print("Server started")
             await asyncio.Future()
+    except KeyboardInterrupt:
+        pass
     finally:
         picam2.stop_recording()
-    
+        picam2.close()
 
 asyncio.run(main())
 
