ed6e9729448fed4197a5452c9e8dcf599aba62ed
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 19:46:54 +0300

    pls

--- Blueprints/Testing/send_mjpeg_ws.py
+++ Blueprints/Testing/send_mjpeg_ws.py
@@ -2,7 +2,6 @@
 import asyncio
 import websockets
 import sys
-# import io
 '''
     Intended to be run from terminal as: 
     raspivid -vf -hf -fps 30 -t 0 -l -w 1920 -h 1080 --codec MJPEG -o - | python3 send_mjpeg_ws.py
@@ -10,33 +9,20 @@ import sys
 async def handle_stream(websocket):
     try:
         marker = b'\xff\xd8'
-        trailer = b'\xff\xd9'
         while True:
-            # image_buffer = io.BytesIO()
-            data = sys.stdin.buffer.readline() # Read from stdin until boundary marker
-            # print(data)
+            data = sys.stdin.buffer.readline()
             if not data:
-                break        
-            if data.startswith(marker): # start of JPEG frame
-                print("Start of JPEG frame")
+                break
+            if data.startswith(marker):
+                print("Got new JPEG")
                 frame = bytearray()
                 while True:
                     data = sys.stdin.buffer.readline()
                     if data.startswith(marker):
-                        # Send the JPEG data over the WebSocket connection
                         await websocket.send(frame)
+                        print("Sent frame to client")
                         break
                     frame.extend(data)
-                    # data = sys.stdin.buffer.readline()
-                    # # print(data)
-                    # image_buffer.seek(0)
-                    # image_buffer.write(data)
-                    # image_buffer.truncate()
-                    # frame = image_buffer.getvalue()
-                    # if data.startswith(trailer): # end of JPEG frame
-                    #     await websocket.send(frame)
-                    #     print("\n\tSent frame\n")
-                    #     break
     except websockets.exceptions.ConnectionClosedError:
         pass
 
