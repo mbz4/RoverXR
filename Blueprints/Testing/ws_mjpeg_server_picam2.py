9b0b3c50064fd41fdd486ceedfa9b88d69204486
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-29 14:03:28 +0300

    ws_mjpeg_server dev1

--- Blueprints/Testing/ws_mjpeg_server_picam2.py
+++ Blueprints/Testing/ws_mjpeg_server_picam2.py
@@ -8,14 +8,14 @@
 
 import io
 import logging
-from websockets.server import serve
 import asyncio
+import websockets
 from threading import Condition
+
 from picamera2 import Picamera2
 from picamera2.encoders import MJPEGEncoder, Quality
 from picamera2.outputs import FileOutput
-from libcamera import controls
-
+from libcamera import controls, Transform
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -30,42 +30,21 @@ class StreamingOutput(io.BufferedIOBase):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+        try:
+            while True:
+                with output.condition:
+                    output.condition.wait()
+                    frame = output.frame
+                self.wfile.write(b'--FRAME\r\n')
+                self.send_header('Content-Type', 'image/jpeg')
+                self.send_header('Content-Length', len(frame))
+                self.end_headers()
+                self.wfile.write(frame)
+                self.wfile.write(b'\r\n')
+        except Exception as e:
+            logging.warning(
+                'Removed streaming client %s: %s',
+                self.client_address, str(e))
 
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
@@ -75,7 +54,8 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 #picam setup
 picam2 = Picamera2()
 picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)})) # defaul framerate=30fps
+picam2.video_configuration.controls.FrameRate = 30.0
+picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}, transform=Transform(hflip=1, vflip=1))) # defaul framerate=30fps
 output = StreamingOutput()
 picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.LOW) #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps 
 
