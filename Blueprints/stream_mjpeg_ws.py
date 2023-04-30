8363b0ec8f3a4fd015625ff13648d70202f7d162
mbz <30321314+mbz4@users.noreply.github.com>
2023-04-30 15:04:02 +0300

    modified exception handling, commented everything

--- Blueprints/stream_mjpeg_ws.py
+++ Blueprints/stream_mjpeg_ws.py
@@ -1,55 +1,80 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
+# shebang line - specifies the path to the python interpreter on the pi zero 1 w
+import io # import the io library
+from threading import Condition #  import the condition class
+from websockets.server import serve # import the websockets library
+import asyncio # import the asyncio library
+from picamera2 import Picamera2 # import the picamera2 library
+from picamera2.encoders import MJPEGEncoder, Quality # import the MJPEG encoder and quality settings
+from picamera2.outputs import FileOutput # import the file output
+from libcamera import Transform # import the transform class
 
-import io
-from threading import Condition
-from websockets.server import serve
-import asyncio
-import time
-from picamera2 import Picamera2
-from picamera2.encoders import MJPEGEncoder, Quality
-from picamera2.outputs import FileOutput
-from libcamera import Transform
+'''
+    Streaming buffer class:
+    this class is used to store the frame from the camera to be sent to the client
+'''
+class StreamingOutput(io.BufferedIOBase): # inherit from BufferedIOBase
+    def __init__(self): # constructor
+        self.frame = None # used to store the frame
+        self.condition = Condition() # used to notify the client when a new frame is available
 
-class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = Condition()
+    def write(self, buf): # write function
+        with self.condition: # wait for the client to request a frame
+            self.frame = buf # store the frame
+            self.condition.notify_all() # notify the client
 
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
+'''
+    Picamera2 setup:
+    used to configure the camera and start recording
+'''
+picam2 = Picamera2() # create a new camera object
 
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)}, transform=Transform(hflip=1, vflip=1)))
-time.sleep(2)
-output = StreamingOutput()
-picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.VERY_LOW) #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps 
+picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)}, # set the resolution
+                                                   transform=Transform(hflip=1, vflip=1))) # apply transforms to image
+output = StreamingOutput() # create a new streaming buffer object
+
+picam2.start_recording(MJPEGEncoder(), # use the MJPEG encoder
+                       FileOutput(output), # output the frames to the streaming buffer
+                       Quality.VERY_LOW) #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps 
 print('\033[2;31;43mRecording started\033[0;0m')
 
-async def handle_stream(websocket):
-    global output
-    try:
-        while True:
-            with output.condition:
-                output.condition.wait()
-                frame = output.frame
-            await websocket.send(frame)
-    except Exception as e:
-        print(e)
+'''
+    Handle stream function:
+    used to handle the websocket connection and send the frames to the client
+'''
+async def handle_stream(websocket): # websocket handler
+    global output # use the global output variable
+    try: # try to run the code
+        while True: # run forever
+            with output.condition: # wait for a new frame
+                output.condition.wait() # wait for a new frame
+                frame = output.frame # get the frame
+            await websocket.send(frame) # send the frame to the client
+    except Exception as e: # catch exceptions
+        if e == KeyboardInterrupt: # catch keyboard interrupt
+            pass # do nothing
+        else:
+            print(e) # print the exception
+        
         print('\033[2;31;43m Closing websocket\033[0;0m')
-        await websocket.close()
+        await websocket.close() # close the websocket connection
 
+'''
+    Main function:
+    used to start the server and handle the websocket connection
+'''
 async def main():
     try:
-        async with serve(handle_stream, "0.0.0.0", 3333, ping_interval=None, max_size = 3145728):
+        async with serve(handle_stream, "0.0.0.0", # server address
+                         3333, # server port
+                         ping_interval=None, # ping_interval = None to prevent connection timeout
+                         max_size = 3538944): # max_size = 3.275MB = 27Mbps
             print('\033[2;31;43mServer started\033[0;0m')
-            await asyncio.Future()
-    except KeyboardInterrupt:
-        pass
+            await asyncio.Future() # run forever
+    except KeyboardInterrupt: # catch keyboard interrupt
+        picam2.stop_recording() # gracefully stop recording
+        picam2.close() # shutdown the camera gracefully
+        print('\033[2;31;43mRecording stopped\033[0;0m') # notify user
     finally:
-        picam2.stop_recording()
-        picam2.close()
-        print('\033[2;31;43mRecording stopped\033[0;0m')
         print("\033[2;31;43mExiting...\033[0;0m")
-asyncio.run(main())
+asyncio.run(main()) # start the server
