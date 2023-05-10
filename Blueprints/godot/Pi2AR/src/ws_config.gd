12117fdd85db8c2bf97fb948e461e1eda2fe7287
mbz <30321314+mbz4@users.noreply.github.com>
2023-05-10 15:27:10 +0300

    readme updates

--- Blueprints/godot/Pi2AR/src/ws_config.gd
+++ Blueprints/godot/Pi2AR/src/ws_config.gd
@@ -2,7 +2,7 @@ extends Node
 
 # ws vars
 #var WS_URI : String = "ws://192.168.199.228:3333"
-var WS_URI : String = "ws://192.168.170.228:3333"
+var WS_URI : String = "ws://192.168.8.152:3333"
 #var WS_URI : String = "ws://mbzpi.local:3333"
 var INBOUND_BUFFER_SIZE : int = 3538944 # max_size = 3.275MB = 27Mbps
 # #VERY_LOW=6Mbps, LOW=12Mbps, MEDIUM=18Mbps, HIGH=27Mbps
@@ -13,6 +13,3 @@ var FPS_UPDATE_DELAY : float = 500 # ms
 var SHOW_FRAME_DEBUG : bool = false
 var DEBUG : String
 var FPS : String
-
-
-
